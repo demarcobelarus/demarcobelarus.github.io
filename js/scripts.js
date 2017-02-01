f84afa31624be377c52cb8836cdfcba1eae450e3
Vladzimir Lukyanovich <v.lukyanovich@gmail.com>
2017-02-01 13:39:48 +0300

    add products

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,18 @@ function docInit(){
 }
 $(document).ready(function(){
 	docInit();
+
+	$(".text-bl").removeClass("open");
+	var textNav = $(".nav-text").length;
+	for(var i=0;i<textNav;i++){var openBl = $(".nav-text").eq(i).find(".selected").eq(0).find("a").attr("href");$(openBl).addClass("open");}
+	$(".nav-text a").on("click", function(){
+		if( $(this).parent().hasClass("selected") != true )
+		{
+			var oldBl = $(this).parent().parent().find(".selected").eq(0).find("a").attr("href");$(oldBl).removeClass("open");
+			$(this).parent().parent().find(".selected").eq(0).removeClass("selected");
+			$(this).parent().addClass("selected");
+			var openBl = $(this).attr("href");$(openBl).addClass("open");
+		}
+		return false;
+	});
 });
